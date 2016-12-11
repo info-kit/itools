f8ab6004282fd43700d5b8d4641cc1d5a3a33a2b
avp-ik <biz.infokit@gmail.com>
2016-12-11 20:46:01 +0300

    The first file

---
+++ 2016-11-12_Python_sys_arg.py
@@ -0,0 +1,13 @@
+# Here I would like to research the possibilities of sys library.
+# C:\Python\Programs\Python Lab\2016\2016-11-12_Python_sys_arg.py
+
+# from __future__ import print_function
+import sys
+
+def main():
+	print(dir(sys))
+	print(sys.argv)
+	print('!!!')
+
+if __name__ == '__main__':
+	main()
